d0331a1750cb077732cb7ab72abd8aecda223507
resa <rezaaja2609@gmail.com>
2026-06-01 20:00:59 +0700

    Fix typo: Ubah showLoginForm menjadi showLogin di web.php

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
 });
 
 // 2. Rute Autentikasi (Sistem Login & Logout)
-Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
